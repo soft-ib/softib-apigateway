a9037dc99d46f077c61906921a8a6e7257e196b4
unknown <dhiasaadlaui@gmail.com>
2021-06-20 09:14:53 +0100

    Token forwarding

---
+++ src/main/java/com/softib/gateway/RelayTokenFilter.java
@@ -0,0 +1,40 @@
+package com.softib.gateway;
+
+import java.util.Set;
+
+import org.springframework.stereotype.Component;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+
+@Component
+public class RelayTokenFilter extends ZuulFilter {
+
+	  @Override
+	    public Object run() {
+	        RequestContext ctx = RequestContext.getCurrentContext();
+
+	        // Alter ignored headers as per: https://gitter.im/spring-cloud/spring-cloud?at=56fea31f11ea211749c3ed22
+	        Set<String> headers = (Set<String>) ctx.get("ignoredHeaders");
+	        // We need our JWT tokens relayed to resource servers
+	        headers.remove("authorization");
+	        headers.remove("Authorization");
+
+	        return null;
+	    }
+
+	    @Override
+	    public boolean shouldFilter() {
+	        return true;
+	    }
+
+	    @Override
+	    public String filterType() {
+	        return "pre";
+	    }
+
+	    @Override
+	    public int filterOrder() {
+	        return 10000;
+	    }
+}
